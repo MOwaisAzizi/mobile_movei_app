5ed15d549afe64d703271dc57eef7f3eb4ee34ad
owias azizi <owaisazizi360@gmail.com>
2026-02-02 23:07:38 +0430

    intialize the base files

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      className="flex-1 justify-center items-center bg-white"
+      className="flex-1 justify-center items-center bg-dark-900"
     >
-      <Text className="text-5x">Welcome.</Text>
+      <Text className="text-5x text-dark-200">Welcome</Text>
+      <Link href="/onboarding">CLick to Onboarding</Link>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        accent: "#ABBBFF",
+        light:{
+          '100': '#D6C6FF',
+          '200': '#ABB5DB',
+          '300': '#9CA4AB',
+        },
+        dark:{
+          '100': '#221F3d',
+          '200': '#0f0d23',
+         }
+      }
+    },
   },
   plugins: [],
 }
